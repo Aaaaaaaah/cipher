e900d941c2656ead8cb1c4645ba5160f2c3392ff
trumpet <zh970205@outlook.com>
2016-12-16 23:35:23 +0800

    update

--- count.py
+++ count.py
@@ -4,30 +4,7 @@ import numpy.random
 import traceback
 import ahocorasick
 import argparse
-
-# file
-this_file_name="ciphertext"
-
-# letter => mat_poss
-letter_T=2.0
-base_order="etaoinhsrdlucmwgfypbkvxjzq"
-base_param=[1174913,862434,756008,716854,641296,628461,581186,579195,545403,425327,395178,272732,238199,237851,218206,206612,198625,187488,170499,143028,92313,87447,17096,15347,9200,8102]
-base_mat=[[math.exp(-(math.log(1.*base_param[i]/base_param[j]))**2/letter_T**2) for j in range(26)]for i in range(26)]
-for i in range(26):
-    base_mat[i][i]=0
-flatten=sum(base_mat,[])
-flatten_sum=sum(flatten)
-mat_poss=map(lambda x:x/flatten_sum,flatten)
-
-# word => ac_auto
-word_T=10000000000
-ddict=json.load(open("wordlist/ddict","r"))
-multi_order={i[0]:i[1]*2**len(i[0]) for i in ddict.iteritems()}
-ac_auto = ahocorasick.Automaton()
-for word in multi_order.iterkeys():
-    i=str(word)
-    ac_auto.add_word(i,i)
-ac_auto.make_automaton()
+from mpi4py import MPI
 
 def get_content(file_name):
     content_file=open(file_name,"r")
@@ -76,13 +53,11 @@ def energy_func(data,rule=None):
 def anneal(times=1000,back=None,starter=1):
     if back==None:
         data=get_content(this_file_name)
-        rule=get_init(this_file_name)
-        E = energy_func(data)
-        EP = 10*E
-        EM = E
-        ruleM = rule
-        rules=[rule]
-        Es=[E]
+        ruleM=get_init(this_file_name)
+        EM = energy_func(data)
+        EP = 10*EM
+        rules=[ruleM]
+        Es=[EM]
     else:
         data=get_content(this_file_name)
         Es=back[2]
@@ -104,8 +79,8 @@ def anneal(times=1000,back=None,starter=1):
         while temp_rule in rules:
             temp_rule=mutate_order(temp_rule)
         temp_E=energy_func(data,temp_rule)
-        rules.append(temp_rule)
-        Es.append(temp_E)
+        rules+=comm.allgather(temp_rule)
+        Es+=comm.allgather(temp_E)
         if i!=0:
             print "Step : %i\tE : %d"%(i,temp_E)
         if temp_E>EM:
@@ -116,14 +91,54 @@ def anneal(times=1000,back=None,starter=1):
     return [replace_str(data,ruleM),rules,Es]
 
 if __name__=="__main__":
+    global this_file
+    global mat_poss
+    global ac_auto
+
+    # file
+    this_file_name="ciphertext"
+
+    # letter => mat_poss
+    letter_T=2.0
+    base_order="etaoinhsrdlucmwgfypbkvxjzq"
+    base_param=[1174913,862434,756008,716854,641296,628461,581186,579195,545403,425327,395178,272732,238199,237851,218206,206612,198625,187488,170499,143028,92313,87447,17096,15347,9200,8102]
+    base_mat=[[math.exp(-(math.log(1.*base_param[i]/base_param[j]))**2/letter_T**2) for j in range(26)]for i in range(26)]
+    for i in range(26):
+        base_mat[i][i]=0
+    flatten=sum(base_mat,[])
+    flatten_sum=sum(flatten)
+    mat_poss=map(lambda x:x/flatten_sum,flatten)
+
+    # word => ac_auto
+    word_T=10000000000
+    ddict=json.load(open("wordlist/ddict","r"))
+    multi_order={i[0]:i[1]*2**len(i[0]) for i in ddict.iteritems()}
+    ac_auto = ahocorasick.Automaton()
+    for word in multi_order.iterkeys():
+        i=str(word)
+        ac_auto.add_word(i,i)
+    ac_auto.make_automaton()
+
+    global comm
+    global comm_rank
+    global comm_size
+
+    comm = MPI.COMM_WORLD
+    comm_rank = comm.Get_rank()
+    comm_size = comm.Get_size()
+
     parser = argparse.ArgumentParser(description='cipher')
     parser.add_argument('--times','-t',dest='times',type=int,default=1000,help='times to anneal')
     parser.add_argument('--file','-f',dest='file',required=True,help='file to store',)
     parser.add_argument('--starter','-s',dest='starter',type=float,default=1.)
     args = parser.parse_args()
+
     try:
         back=json.load(open(args.file,"r"))
     except IOError:
         back=None
+
     ans=anneal(times=args.times,back=back,starter=args.starter)
-    json.dump(ans,open(args.file,"w"))
+
+    if comm_rank == 0:
+        json.dump(ans,open(args.file,"w"))
