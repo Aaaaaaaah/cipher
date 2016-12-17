867fdf889775450505aa77d50ef7c361337c0744
trumpet <zh970205@outlook.com>
2016-12-17 20:13:56 +0800

    update

---
+++ test2.sh
@@ -0,0 +1,14 @@
+echo $1 $2
+for i in `seq $1 $2`
+do
+    mpirun -host node4,node5,node6 -ppn 36 python count.py -f db$i.1 -p 1
+    mpirun -host node4,node5,node6 -ppn 36 python count.py -f db$i.2 -p 2
+    mpirun -host node4,node5,node6 -ppn 36 python count.py -f db$i.3 -p 3
+    mpirun -host node4,node5,node6 -ppn 36 python count.py -f db$i.4 -p 4
+    mpirun -host node4,node5,node6 -ppn 36 python count.py -f db$i.5 -p 5
+done
+for i in db*
+do
+    python pic.py -f $i | python -m json.tool | tee p.$i
+done
+
