a8aaf763e3ec5deecee73e67bbc3a420536cfc8a
trumpet <zh970205@outlook.com>
2016-12-17 22:58:46 +0800

    update

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 echo $1 $2
 for i in `seq $1 $2`
 do
-    mpirun -host node1,node2,node3 -ppn 36 python count.py -f db$i.1 -p 1
-    mpirun -host node1,node2,node3 -ppn 36 python count.py -f db$i.2 -p 2
-    mpirun -host node1,node2,node3 -ppn 36 python count.py -f db$i.3 -p 3
-    mpirun -host node1,node2,node3 -ppn 36 python count.py -f db$i.4 -p 4
-    mpirun -host node1,node2,node3 -ppn 36 python count.py -f db$i.5 -p 5
+    mpirun -host $3 -ppn 36 python count.py -f db$i.1 -p 1
+    mpirun -host $3 -ppn 36 python count.py -f db$i.2 -p 2
+    mpirun -host $3 -ppn 36 python count.py -f db$i.3 -p 3
+    mpirun -host $3 -ppn 36 python count.py -f db$i.4 -p 4
+    mpirun -host $3 -ppn 36 python count.py -f db$i.5 -p 5
 done
 for i in db*
 do
